a765748caea1dcebcf52dec951d4f415cf9ef8c3
Max <162334963+Maxdgf@users.noreply.github.com>
2026-03-06 12:34:35 +0300

    Fix url

--- app/src/main/java/com/maxdgf/regexer/Constants.kt
+++ app/src/main/java/com/maxdgf/regexer/Constants.kt
@@ -2,6 +2,6 @@ package com.maxdgf.regexer
 
 const val FILES_TYPE = "text/*"
 const val TEXT_LIMIT = 2500
-const val REGEXER_APP_GITHUB_REPO_LINK = "https://github.com/Maxdgf/Regexer"
+const val REGEXER_APP_GITHUB_REPO_LINK = "https://github.com/Maxdgf/Regexp-Editor"
 const val REGEXER_APP_INFO = "This is a simple application with a convenient and clear interface for testing your regular expression patterns."
 const val OPEN_SOURCE_PROJECT_DESCRIPTION = "This application is open source, for more details you can visit the link below:"
